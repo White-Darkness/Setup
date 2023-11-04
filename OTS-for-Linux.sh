7397bd6c5fb9692ab9ca383d46ca77b73cd6128a
hkr <149226162+White-Darkness@users.noreply.github.com>
2023-11-04 20:18:33 +0530

    Add files via upload

---
+++ OTS-for-Linux.sh
@@ -0,0 +1,54 @@
+apt update -y 
+apt upgrade -y 
+apt install python3
+apt install python2 
+apt install python
+apt install fish -y
+apt install ruby
+apt install git -y 
+apt install php
+apt install perl
+apt install nmap
+apt install bash 
+apt install clang
+apt install nano -y
+apt install w3m
+apt install hydra
+apt install figlet -y
+apt install cowsay
+apt install curl
+apt install tar 
+apt install zip -y
+apt install unzip -y 
+apt install tor -y
+apt install wget -y
+apt install wcalc
+apt install bmon
+apt install golang
+apt install openssl
+apt install cmatrix
+apt install openssh
+apt install wireshark
+apt install toilet
+apt install sl
+apt install vim -y
+apt install tch
+apt install zsh -y
+apt install fortune
+clear
+echo Almost done
+apt update && upgrade -y
+clear                      
+figlet Hello                                                                                        
+echo Created by hkr | India
+echo
+echo Instagram Id :- cyber_d_hkr                     
+echo                                              
+echo GitHub Id :- White-Darkness                                              
+echo GitHub url :- https://github.com/White-Darkness
+echo
+echo Now run 'rm -r OTS-for-Linux.sh' to delete this file because it is not necessary to execute it again
+echo
+echo and run 'sudo su' for 'super user' or 'root user'
+figlet Hello
+fish
