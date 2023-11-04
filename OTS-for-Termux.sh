097086d677c18b3916379458880a0beaf559b349
hkr <149226162+White-Darkness@users.noreply.github.com>
2023-11-04 20:19:19 +0530

    Add files via upload

---
+++ OTS-for-Termux.sh
@@ -0,0 +1,52 @@
+apt update -y 
+apt upgrade -y 
+pkg install python3
+pkg install python2 
+pkg install python
+pkg install fish -y
+pkg install ruby
+pkg install git -y 
+pkg install php
+pkg install perl
+pkg install nmap
+pkg install bash 
+pkg install clang
+pkg install nano -y
+pkg install w3m
+pkg install hydra
+pkg install figlet -y
+pkg install cowsay
+pkg install curl
+pkg install tar 
+pkg install zip -y
+pkg install unzip -y 
+pkg install tor -y
+pkg install wget -y
+pkg install wcalc
+pkg install bmon
+pkg install golang
+pkg install openssl
+pkg install cmatrix
+pkg install openssh
+pkg install wireshark
+pkg install toilet
+pkg install sl
+pkg install vim -y
+pkg install tch
+pkg install zsh -y
+pkg install fortune
+clear
+echo Almost done
+apt update && upgrade -y
+clear                                             
+figlet Hello                                                                                        
+echo Created by hkr | India
+echo                                              
+echo Instagram Id :- cyber_d_hkr                                                                    
+echo
+echo GitHub Id :- White-Darkness            
+echo GitHub url :- 'https://github.com/White-Darkness'
+echo
+echo Now run 'rm -r OTS-for-Termux.sh' to delete this file because it is not necessary to execute it again
+echo
+fish
